b45d0abf90efaa864879bdcbdb050ca50428680b
Ben <46194536+Ahebi@users.noreply.github.com>
2022-11-02 10:27:48 +0100

    updtate

--- p7-kasa-react/src/pages/A_Propos.js
+++ p7-kasa-react/src/pages/A_Propos.js
@@ -1,3 +1,8 @@
+import Tag from "../components/Tag";
 export default function A_propos() {
-    return <div> ici il y aura la page A Propos</div>;
+    return (
+        <div>
+            ici test du components "Tag" <Tag />
+        </div>
+    );
 }
