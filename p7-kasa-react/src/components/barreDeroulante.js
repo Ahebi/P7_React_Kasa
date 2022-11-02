feb220624574238da0bb0b621952bad472a96e4d
Ben <46194536+Ahebi@users.noreply.github.com>
2022-11-02 17:31:35 +0100

    post-session

--- p7-kasa-react/src/components/barreDeroulante.js
+++ p7-kasa-react/src/components/barreDeroulante.js
@@ -1,11 +1,16 @@
+import { useEffect } from "react";
 import VectorFlecheBas from "../img/VectorFlecheBas.png";
 
-export default function barreDeroulante() {
-    let boutonFleche = document.getElementsByClassName(
-        "barreDeroulante_fleche"
-    );
-    boutonFleche.addEvenetListener("click", function () {
-        boutonFleche.classList.toggle("rotate");
+export default function BarreDeroulante() {
+    useEffect(() => {
+        let bouton = document.querySelectorAll(".barreDeroulante_fleche")[0];
+        console.log(bouton);
+
+        bouton.addEventListener("click", function (event) {
+            console.dir(event.target.classList);
+            event.target.classList.add("rotate");
+            console.log(event.target);
+        });
     });
 
     return (
@@ -20,12 +25,12 @@ export default function barreDeroulante() {
             </div>
             <div className="barreDeroulante_page">
                 <p>Climatisation </p>
-                <p> Wi-Fi</p>
+                <p>Wi-Fi</p>
                 <p>Cuisine</p>
                 <p>Espace de travail</p>
-                <p> Fer à repasser</p>
+                <p>Fer à repasser</p>
                 <p>Sèche-cheveux</p>
-                <p> Cintres</p>
+                <p>Cintres</p>
             </div>
         </div>
     );
